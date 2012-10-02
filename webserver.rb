c5ad7fc1548aed0cd0b89081c6aa7582c53f709a
Blake Johnson <blake41@@gmail.com>
2012-10-02 16:01:48 -0400

    open a new thread per client

--- webserver.rb
+++ webserver.rb
@@ -2,12 +2,14 @@ require 'socket'
 puts 'starting up server'
 server = TCPServer.new(8080)
 while session = server.accept
-	puts "connection established from #{session.peeraddr[2]} at
-		#{session.peeraddr[3]}"
-	while input = session.gets
-		puts input
-	# session.puts "Server: Welcome #{session.peeraddr[2]}\n"
-	# puts "log: sending goodbye"
-	# session.puts "Server: Goodbye\n"
+	Thread.new do
+		puts "connection established from #{session.peeraddr[2]} at
+			#{session.peeraddr[3]}"
+		while input = session.gets
+			puts input
+		# session.puts "Server: Welcome #{session.peeraddr[2]}\n"
+		# puts "log: sending goodbye"
+		# session.puts "Server: Goodbye\n"
+		end
 	end
 end
