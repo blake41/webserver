9c0c49ebce44548af548cff014d9cdd1332193a6
Blake Johnson <blake41@@gmail.com>
2012-10-02 18:36:25 -0400

    pass signals from master to children

--- webserver.rb
+++ webserver.rb
@@ -2,13 +2,22 @@ require 'socket'
 puts 'starting up server'
 
 server = TCPServer.new(8080)
-loop do 
-	session = server.accept
-	if fork
-		puts "connection established from #{session.peeraddr[2]} at
-			#{session.peeraddr[3]}"
-		while input = session.gets
-			puts input
+
+[:INT, :QUIT].each do |signal|
+	Signal.trap(signal) {
+		wpids.each { |wpid| Process.kill(signal, wpid)}
+	}
+end
+
+wpids = []
+5.times do
+	wpids << fork do
+		loop do
+			session = server.accept
+			while input = session.gets
+				puts "#{input} processed by #{Process.pid}"
+			end
 		end
 	end
 end
+Process.waitall
