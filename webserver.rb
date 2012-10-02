b700b62f54da195fd687c3998f6580ed8a524d43
Blake Johnson <blake41@@gmail.com>
2012-10-02 16:47:57 -0400

    fork 2 processes, if we connect with more than 3 clients no one is there to respond

--- webserver.rb
+++ webserver.rb
@@ -1,17 +1,17 @@
 require 'socket'
 puts 'starting up server'
-
 server = TCPServer.new(8080)
+2.times do
+	break unless fork
+end
 loop do 
 	session = server.accept
-	if fork
-		puts "connection established from #{session.peeraddr[2]} at
-			#{session.peeraddr[3]}"
-		while input = session.gets
-			puts input
-		# session.puts "Server: Welcome #{session.peeraddr[2]}\n"
-		# puts "log: sending goodbye"
-		# session.puts "Server: Goodbye\n"
-		end
+	puts "connection established from #{session.peeraddr[2]} at
+		#{session.peeraddr[3]}"
+	while input = session.gets
+		puts "we got #{input} and #{Process.pid} responded"
+	# session.puts "Server: Welcome #{session.peeraddr[2]}\n"
+	# puts "log: sending goodbye"
+	# session.puts "Server: Goodbye\n"
 	end
 end
