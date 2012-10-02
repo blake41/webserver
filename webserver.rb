fea744baf8ce608217663fccf5c2707bb30be5d4
Blake Johnson <blake41@@gmail.com>
2012-10-02 15:59:55 -0400

    server shouldnt disconnect when the client does

--- webserver.rb
+++ webserver.rb
@@ -1,12 +1,13 @@
 require 'socket'
 puts 'starting up server'
 server = TCPServer.new(8080)
-session = server.accept
+while session = server.accept
 	puts "connection established from #{session.peeraddr[2]} at
 		#{session.peeraddr[3]}"
 	while input = session.gets
-	puts input
+		puts input
 	# session.puts "Server: Welcome #{session.peeraddr[2]}\n"
 	# puts "log: sending goodbye"
 	# session.puts "Server: Goodbye\n"
+	end
 end
