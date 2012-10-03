ad781e7ecf46936bf3b71286b386c176dc949c8f
Blake Johnson <blake41@@gmail.com>
2012-10-03 18:27:32 -0400

    parser

---
+++ parser.rb
@@ -0,0 +1,24 @@
+class Parser
+
+	TEMP = "GET / HTTP/1.1\r\n"
+
+	attr_accessor :http_strings, :headers
+
+	def initialize(http_strings)
+		@http_strings = http_strings
+		debugger
+		self.http_strings.collect(&:chomp)
+	end
+
+	def parse
+		debugger
+		self.headers[:method] = self.parse_method(self.http_strings[0])[0]
+		return self.headers
+	end
+
+	def parse_method(method_string)
+		debugger
+		method_string.split(/\//).collect(&:strip)
+	end
+
+end
